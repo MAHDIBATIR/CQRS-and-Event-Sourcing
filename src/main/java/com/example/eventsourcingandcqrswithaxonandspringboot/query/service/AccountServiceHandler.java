014b55c21b52d27b37dec7350150c63b80c536a2
MAHDIBATIR <elbatirelmahdi@gmail.com>
2025-12-06 12:47:52 +0100

    Côté Requête - Gestionnaires d'événements (Projections)

---
+++ src/main/java/com/example/eventsourcingandcqrswithaxonandspringboot/query/service/AccountServiceHandler.java
@@ -0,0 +1,91 @@
+package com.example.eventsourcingandcqrswithaxonandspringboot.query.service;
+
+import com.example.eventsourcingandcqrswithaxonandspringboot.commonapi.enums.AccountStatus;
+import com.example.eventsourcingandcqrswithaxonandspringboot.commonapi.enums.OperationType;
+import com.example.eventsourcingandcqrswithaxonandspringboot.commonapi.events.AccountActivatedEvent;
+import com.example.eventsourcingandcqrswithaxonandspringboot.commonapi.events.AccountCreatedEvent;
+import com.example.eventsourcingandcqrswithaxonandspringboot.commonapi.events.AccountCreditedEvent;
+import com.example.eventsourcingandcqrswithaxonandspringboot.commonapi.events.AccountDebitedEvent;
+import com.example.eventsourcingandcqrswithaxonandspringboot.query.entities.Account;
+import com.example.eventsourcingandcqrswithaxonandspringboot.query.entities.AccountOperation;
+import com.example.eventsourcingandcqrswithaxonandspringboot.query.repositories.AccountRepository;
+import com.example.eventsourcingandcqrswithaxonandspringboot.query.repositories.OperationRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+@Service
+@Transactional
+@Slf4j
+public class AccountServiceHandler {
+    private AccountRepository accountRepository;
+    private OperationRepository operationRepository;
+    private org.axonframework.queryhandling.QueryUpdateEmitter queryUpdateEmitter;
+
+    public AccountServiceHandler(AccountRepository accountRepository, OperationRepository operationRepository, org.axonframework.queryhandling.QueryUpdateEmitter queryUpdateEmitter) {
+        this.accountRepository = accountRepository;
+        this.operationRepository = operationRepository;
+        this.queryUpdateEmitter = queryUpdateEmitter;
+    }
+
+    @EventHandler
+    public void on(AccountCreatedEvent event) {
+        log.info("AccountCreatedEvent received");
+        Account account = Account.builder()
+                .id(event.getId())
+                .balance(event.getInitialBalance())
+                .currency(event.getCurrency())
+                .status(AccountStatus.valueOf(event.getStatus()))
+                .build();
+        accountRepository.save(account);
+    }
+
+    @EventHandler
+    public void on(AccountActivatedEvent event) {
+        log.info("AccountActivatedEvent received");
+        Account account = accountRepository.findById(event.getId()).orElse(null);
+        if (account != null) {
+            account.setStatus(AccountStatus.valueOf(event.getStatus()));
+            accountRepository.save(account);
+        }
+    }
+
+    @EventHandler
+    public void on(AccountCreditedEvent event) {
+        log.info("AccountCreditedEvent received");
+        Account account = accountRepository.findById(event.getId()).orElse(null);
+        if (account != null) {
+            account.setBalance(account.getBalance() + event.getAmount());
+            accountRepository.save(account);
+            AccountOperation operation = AccountOperation.builder()
+                    .amount(event.getAmount())
+                    .operationDate(new Date()) // Should ideally come from event metadata
+                    .type(OperationType.CREDIT)
+                    .account(account)
+                    .build();
+            operationRepository.save(operation);
+            queryUpdateEmitter.emit(m -> ((com.example.eventsourcingandcqrswithaxonandspringboot.commonapi.queries.GetAccountQuery) m.getPayload()).getId().equals(event.getId()), account);
+        }
+    }
+
+    @EventHandler
+    public void on(AccountDebitedEvent event) {
+        log.info("AccountDebitedEvent received");
+        Account account = accountRepository.findById(event.getId()).orElse(null);
+        if (account != null) {
+            account.setBalance(account.getBalance() - event.getAmount());
+            accountRepository.save(account);
+            AccountOperation operation = AccountOperation.builder()
+                    .amount(event.getAmount())
+                    .operationDate(new Date())
+                    .type(OperationType.DEBIT)
+                    .account(account)
+                    .build();
+            operationRepository.save(operation);
+            queryUpdateEmitter.emit(m -> ((com.example.eventsourcingandcqrswithaxonandspringboot.commonapi.queries.GetAccountQuery) m.getPayload()).getId().equals(event.getId()), account);
+        }
+    }
+}
